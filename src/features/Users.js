93686abc0fa10a15bed7937386937e12962c55a0
ffiruz <ffiruz@seckin.com.tr>
2023-03-29 17:06:37 +0300

    delete function added with redux

--- src/features/Users.js
+++ src/features/Users.js
@@ -13,10 +13,13 @@ export const userSlice = createSlice ({
         addUser: (state, action) => {
            state.value.push(action.payload);
 
+        },
+        deleteUser: (state, action) => {
+            state.value = state.value.filter((user) => user.id != action.payload)
         }
     }
 })
 
 
-export const {addUser} = userSlice.actions
+export const {addUser,deleteUser} = userSlice.actions
 export default userSlice.reducer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 
 import { useSelector, useDispatch } from "react-redux"
-import { addUser } from "./features/Users"
+import { addUser, deleteUser } from "../features/Users"
+import { useState } from "react"
 
 
 
@@ -8,12 +9,17 @@ import { addUser } from "./features/Users"
 export default function Home() {
   const dispatch = useDispatch()
   const userList = useSelector((state)=> state.users.value)
+
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
  return <>
       <div>
-        <input type='text ' placeholder="Name.." />
-        <input type='text ' placeholder="UserName.." />
+        <input type='text ' placeholder="Name.."  onChange={(e)=> setName(e.target.value)}/>
+        <input type='text ' placeholder="UserName.."onChange={(e)=> setUsername(e.target.value)} />
         <button onClick={()=>{
-          dispatch(addUser({}))
+          dispatch(addUser({id: userList[userList.length-1].id + 1,  
+            name, 
+            username, }))
         }} >Add User</button>
       </div>
       <div>
@@ -21,6 +27,12 @@ export default function Home() {
           return <div key={user.id}>
           <h1>{user.name}</h1>
           <h1>{user.username}</h1>
+          <input type = "text" placeholder="new user name" />
+          <button onClick={() =>(
+            dispatch(deleteUser({id:user.id}))
+          )}>
+           delete
+          </button>
           </div>
         })}
       </div>
